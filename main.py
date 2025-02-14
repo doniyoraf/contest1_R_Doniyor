a68c17374de7afb8583e99d01e11a09f5755b7bf
doniyoraf <161677762+doniyoraf@users.noreply.github.com>
2025-02-14 23:20:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,42 @@ from task1 import kwargsAcceptFun
 from task2 import typeBasedTransformer
 from task3 import decorator_1
 
+print("Vazifa 1 Natijasi:")
+print(kwargsAcceptFun(ism="Doniyor", yosh=21, mamlakat="O‘zbekiston"))
 
-print("Task 1 Output:")
-print(kwargsAcceptFun(name="Alice", age=25, country="USA"))
-
-
-print("\nTask 2 Output:")
-print(typeBasedTransformer(a=5, b="Hello", c=True, d=[1, 2, 3], e={"x": 10, "y": 20}))
+print("\nVazifa 2 Natijasi:")
+print(typeBasedTransformer(
+    son=7, 
+    matn="Salom", 
+    mantiqiy=True, 
+    royxat=[10, 20, 30], 
+    lugat={"a": 1, "b": 2}
+))
 
 
 @decorator_1
-def func():
-    print("I am ready to Start")
-    result = 0
+def funksiya():
+    print("Men ishga tayyorman")
+    natija = 0
     n = random.randint(10, 751)
     for i in range(n):
-        result += (i**2)
+        natija += (i**2)
+
 
 @decorator_1
-def funx(n=2, m=5):
-    print("I am ready to do serious stuff")
-    max_val = float('-inf')
+def boshqa_funksiya(n=2, m=5):
+    print("Men jiddiy ish qilishga tayyorman")
+    eng_katta = float('-inf')
     n = random.randint(10, 751)
-    res = [pow(i, 2) for i in range(n)]
-    for i in res:
-        if i > max_val:
-            max_val = i
+    natijalar = [pow(i, 2) for i in range(n)]
+    for qiymat in natijalar:
+        if qiymat > eng_katta:
+            eng_katta = qiymat
+
 
 if __name__ == "__main__":
-    func()
-    funx()
-    func()
-    funx()
-    func()
+    funksiya()
+    boshqa_funksiya()
+    funksiya()
+    boshqa_funksiya()
+    funksiya()
