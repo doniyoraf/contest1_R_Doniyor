a3210f56fec8da14d51f53c18be0cdab47b902bc
doniyoraf <161677762+doniyoraf@users.noreply.github.com>
2025-02-14 23:22:19 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,6 +5,6 @@ def decorator_1(funks):
         boshlanish_vaqti = time.time()
         natija = funks(*argumentlar, **kalit_argumentlar)
         tugash_vaqti = time.time()
-        print(f"{func.__name__} call executed in {tugash_vaqti - boshlanish_vaqti:.6f} sec")
+        print(f"{funks.__name__} call executed in {tugash_vaqti - boshlanish_vaqti:.6f} sec")
         return natija
     return doniyor
