baacc89b24dbda1c4c69e743119bef9667d4a063
doniyoraf <161677762+doniyoraf@users.noreply.github.com>
2025-02-14 23:14:40 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,10 +1,10 @@
 import time
 
-def decorator_1(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"{func.__name__} call executed in {end_time - start_time:.4f} sec")
-        return result
-    return wrapper
+def decorator_1(funks):
+    def doniyor(*argumentlar, **kalit_argumentlar):
+        boshlanish_vaqti = time.time()
+        natija = funks(*argumentlar, **kalit_argumentlar)
+        tugash_vaqti = time.time()
+        print(f"{func.__name__} call executed in {tugash_vaqti - boshlanish_vaqti:.6f} sec")
+        return natija
+    return doniyor
