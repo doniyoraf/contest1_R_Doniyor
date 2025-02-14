333f79d104a2ff52586f47b6b8512d2739f10456
doniyoraf <161677762+doniyoraf@users.noreply.github.com>
2025-02-14 23:16:25 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,18 +1,18 @@
-def typeBasedTransformer(**kwargs):
-    ozgargan = {}
+def typeBasedTransformer(**argumentlar):
+    ozgartirdim = {}
     
-    for kalit, qiymat in kwargs.items():
+    for kalit, qiymat in argumentlar.items():
         if isinstance(qiymat, bool):
-            transformed[kalit] = not qiymat
+            ozgartirdim[kalit] = not qiymat
         elif isinstance(qiymat, str):
-            transformed[kalit] = qiymat[::-1]
+            ozgartirdim[kalit] = qiymat[::-1]
         elif isinstance(qiymat, (int, float)):
-            transformed[kalit] = value ** 2
+            ozgartirdim[kalit] = value ** 2
         elif isinstance(qiymat, (list, tuple)):
-            transformed[kalit] = qiymat[::-1]
+            ozgartirdim[kalit] = qiymat[::-1]
         elif isinstance(qiymat, dict) and len(set(qiymat.values())) == len(qiymat.values()):
-            ozgargan[kalit] = {v: k for k, v in qiymat.items()}
+            ozgartirdim[kalit] = {v: k for k, v in qiymat.items()}
         else:
-            ozgargan[kalit] = qiymat
+            ozgartirdim[kalit] = qiymat
             
-    return ozgargan
+    return ozgartirdim
